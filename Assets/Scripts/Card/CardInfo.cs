77cfe5cdbc03bc631e880ca39f78bc22ff8e46b8
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2020-12-06 15:34:47 +0900

    カード作成用クラスの作成

---
+++ Assets/Scripts/Card/CardInfo.cs
@@ -0,0 +1,20 @@
+﻿public class CardInfo
+{
+    public string name { get; set; }
+    public NameColor nameColor { get; set; }
+    public NameSize nameSize { get; set; }
+    public Frame frame { get; set; }
+    public MonsterAttribute monsterAttribute { get; set; }
+    public MonsterLevel monsterLevel { get; set; }
+    public string attack { get; set; }
+    public string defence { get; set; }
+    public string monsterText { get; set; }
+    public string text { get; set; }
+    public TextSize textSize { get; set; }
+    public string pendulumText { get; set; }
+    public PendulumTextSize pendulumTextSize { get; set; }
+    public PendulumNumBlue pendulumNumBlue { get; set; }
+    public PendulumNumRed pendulumNumRed { get; set; }
+    public LinkNum linkNum { get; set; }
+    public int linkPosition { get; set; }
+}
---
+++ Assets/Scripts/Enums/CardEnums.cs
@@ -0,0 +1,144 @@
+﻿public enum NameColor
+{
+    Black = 0,
+    White,
+    Shilver,
+    Gold,
+    Red
+}
+
+public enum NameSize
+{
+    Medium,
+    Small
+}
+
+public enum Frame
+{
+    NormalMonster,
+    EffectMonster,
+    RitualMonster,
+    FusionMonster,
+    SynchroMonster,
+    XyzMonster,
+    PendulumNormalMonster,
+    PendulumEffectMonster,
+    PendulumRitualMonster,
+    PendulumFusionMonster,
+    PendulumSynchroMonster,
+    PendulumXyzMonster,
+    LinkMonster,
+    NormalSpell,
+    QuickPlaySpell,
+    ContinuousSpell,
+    EquipSpell,
+    RitualSpell,
+    FieldSpell,
+    NormalTrap,
+    ContinuousTrap,
+    CounterTrap,
+    Token,
+    TokenOptional
+}
+
+public enum MonsterAttribute
+{
+    Darkness,
+    Light,
+    Earth,
+    Water,
+    Flame,
+    Wind,
+    God
+}
+
+public enum MonsterLevel
+{
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Sevel,
+    Eight,
+    Nine,
+    Ten,
+    Elevel,
+    Twelve
+}
+
+public enum TextSize
+{
+    Large,
+    Medium,
+    Small
+}
+
+public enum PendulumTextSize
+{
+    Medium,
+    Small
+}
+
+public enum PendulumNumBlue
+{
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Sevel,
+    Eight,
+    Nine,
+    Ten,
+    Elevel,
+    Twelve,
+    Thirteen
+}
+
+public enum PendulumNumRed
+{
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Sevel,
+    Eight,
+    Nine,
+    Ten,
+    Elevel,
+    Twelve,
+    Thirteen
+}
+
+public enum LinkNum
+{
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight
+}
+
+public enum LinkPosition
+{
+    UpperLeft,
+    Up,
+    UpperRight,
+    Left,
+    Right,
+    LowerLeft,
+    Down,
+    LowerRight
+}
