544f6702ca53a0500750d916903a640f4d2348b3
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2021-06-10 21:51:18 +0900

    動画再生時に確認ダイアログを表示するように

--- Assets/Scripts/UI/Window/UIScript/HomeWindowUIScript.cs
+++ Assets/Scripts/UI/Window/UIScript/HomeWindowUIScript.cs
@@ -17,6 +17,13 @@ public class HomeWindowUIScript : WindowBase
             .Subscribe();
 
         _rewardButton.OnClickIntentAsObservable()
+            .SelectMany(_ => CommonDialogFactory.Create(new CommonDialogRequest()
+            {
+                title = "確認",
+                body = "広告を再生して本日の保存回数制限を解除します",
+                type = CommonDialogType.Selection,
+            }))
+            .Where(res => res.responseType == CommonDialogResponseType.Yes)
             .Do(_ =>
             {
                 var action = new Action(() =>
